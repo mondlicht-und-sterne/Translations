0020176dfab156145dc25e4471e94259016dbe65
mondstern <61406324+mondlicht-und-sterne@users.noreply.github.com>
2022-08-04 19:43:11 +0200

    Create de-GER.ts

---
+++ de-GER.ts
@@ -0,0 +1,67 @@
+import { Translation } from 'types/Translation';
+
+const de_GER: Translation = {
+  // DASHBOARD
+  CREDIT: 'Kredit',
+  DEBIT: 'Lastschrift',
+  RECENT_TRANSACTIONS: 'Aktuelle Transaktionen',
+  MY_ACCOUNTS: 'Meine Konten',
+
+  // ADD ACCOUNT
+  ADD_ACCOUNT: 'Konto hinzufügen',
+  ACCOUNT_NAME: 'Name des Kontos',
+  INITIAL_AMOUNT: 'Ausgangsbetrag',
+  CREATE_ACCOUNT: 'CKonto erstellen',
+
+  // EDIT ACCOUNT
+  EDIT_ACCOUNT: 'Konto bearbeiten',
+  UPDATE_ACCOUNT: Konto aktualisieren',
+
+  // ACCOUNT DETAILS
+  ACCOUNT_DETAILS: 'Konto Details',
+  INITIAL_BALANCE: 'Anfangsbestand',
+  DELETE_ACCOUNT: 'Konto löschen?',
+  DELETE_ACCOUNT_INFO:
+    'Dies löscht dauerhaft das Konto {{accountName}} mit der Brieftasche {{transactionCount}} und verknüpfte Transaktionen.',
+
+  // NEW TRANSACTION
+  NEW_TRANSACTION: 'Neue Transaktion',
+  CREATE_TRANSACTION: 'Transaktion erstellen',
+  CATEGORY: 'Kategorie',
+  SOURCE_ACCOUNT: 'Herkunft Konto',
+  DESTINATION_ACCOUNT: 'Ziel-Konto',
+  TRANSFER: 'Übertragung',
+  SHORT_DESCRIPTION: 'Kurzbeschreibung',
+  AMOUNT: 'Betrag',
+
+  // EDIT TRANSACTION
+  EDIT_TRANSACTION: 'Transaktion bearbeiten',
+  UPDATE_TRANSACTION: 'Transaktion aktualisieren',
+
+  // TRANSACTION DETAILS
+  TRANSACTION_DETAILS: 'Angaben zur Transaktion',
+  ACCOUNT: 'Konto',
+  DATE_CREATED: 'Datum der Erstellung',
+  DATE_UPDATED: 'Datum aktualisiert',
+  DELETE_TRANSACTION: 'Transaktion löschen?',
+  DELETE_TRANSACTION_INFO:
+    'Dadurch wird der Transaktionsdatensatz endgültig gelöscht.',
+  KEEP: 'Behalten',
+  DELETE: 'Löschen',
+
+  // Transactions
+  TRANSACTIONS: 'Transaktionen',
+  SEARCH: 'Suche',
+  DATE_RANGE: 'Datumsbereich',
+  SHOW_ALL: Alle anzeigen',
+  EXPORT: 'Exportieren',
+
+  // SETTINGS
+  SETTINGS: 'Einstellungen',
+  TRANSLATION_NAME: 'Deutsch (DE)',
+  CURRENCY: 'Währung',
+  LANGUAGE: 'Sprache',
+  THEME: 'Thema',
+  THEME_LIGHT: 'Hell',
+  THEME_DARK: 'Dunkel',
+};
